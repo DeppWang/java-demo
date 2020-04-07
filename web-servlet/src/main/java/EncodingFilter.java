447f887524e392a39e99f9340125bab5c7f1a299
DeppWang <deppwxq@gmail.com>
2020-04-07 16:26:20 +0800

    first commit

---
+++ web-servlet/src/main/java/EncodingFilter.java
@@ -0,0 +1,18 @@
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
+import java.io.IOException;
+
+@WebFilter(urlPatterns = "/*")
+public class EncodingFilter implements Filter {
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+            throws IOException, ServletException {
+        System.out.println("EncodingFilter:doFilter");
+        request.setCharacterEncoding("UTF-8");
+        response.setCharacterEncoding("UTF-8");
+        chain.doFilter(request, response);
+    }
+}
